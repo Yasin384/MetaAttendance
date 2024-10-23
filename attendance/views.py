c5e28677dd5525db3848609a549eadb73ef84fa5
yasin <asinarstanbekov51@gmail.com>
2024-10-23 17:02:28 +0600

    Восстановлен файл

--- attendance/views.py
+++ attendance/views.py
@@ -231,33 +231,50 @@ def home(request):
 # Admin dashboard view
 
 
-@login_required
 def admin_dashboard(request):
+    # Проверка, является ли пользователь администратором
     if not request.user.is_staff:
         return redirect('home')
 
-    total_students = Student.objects.count()
-    total_teachers = Teacher.objects.count()
-    total_faculties = Faculty.objects.count()
+    # Текущая дата
+    today = timezone.now().date()
 
-    teachers = Teacher.objects.all().annotate(
-        total_days=Count('user__attendance', distinct=True),
-        present_days=Count('user__attendance', filter=Q(user__attendance__status='present')),
-        absent_days=Count('user__attendance', filter=Q(user__attendance__status='absent')),
+    # Подсчет присутствующих и отсутствующих студентов
+    students_present_today = Student.objects.filter(
+        user__attendance__status='present', 
+        user__attendance__date=today
+    ).count()
+    
+    students_absent_today = Student.objects.filter(
+        user__attendance__status='absent', 
+        user__attendance__date=today
+    ).count()
+
+    # Подсчет присутствующих и отсутствующих учителей
+    teachers_present_today = Teacher.objects.filter(
+        user__attendance__status='present', 
+        user__attendance__date=today
+    ).count()
+    
+    teachers_absent_today = Teacher.objects.filter(
+        user__attendance__status='absent', 
+        user__attendance__date=today
+    ).count()
+
+    # Информация о факультетах: количество студентов и присутствующих студентов
+    faculties = Faculty.objects.annotate(
+        total_students=Count('students'),
+        present_today=Count('students__user__attendance', filter=Q(students__user__attendance__status='present', students__user__attendance__date=today))
     )
 
-    faculty_attendance = Faculty.objects.annotate(
-        present_days=Count('students__user__attendance', filter=Q(students__user__attendance__status='present'))
-    ).order_by('-present_days')
-
     context = {
-        'total_students': total_students,
-        'total_teachers': total_teachers,
-        'total_faculties': total_faculties,
-        'teachers': teachers,
-        'faculties': Faculty.objects.all(),
-        'faculty_attendance': faculty_attendance,
+        'students_present_today': students_present_today,
+        'students_absent_today': students_absent_today,
+        'teachers_present_today': teachers_present_today,
+        'teachers_absent_today': teachers_absent_today,
+        'faculties': faculties,
     }
+
     return render(request, 'attendance/admin_dashboard.html', context)
 
 
